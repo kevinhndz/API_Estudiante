4a1096bd986d85eddd43341b49482ec5db737596
Kevin <kh925063@gmail.com>
2026-08-08 14:02:14 -0600

    Configurar Alembic con variables de entorno y limpiar archivos de BD

--- alembic/env.py
+++ alembic/env.py
@@ -1,37 +1,32 @@
 from logging.config import fileConfig
 import os
 from dotenv import load_dotenv
-
-
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
+from sqlalchemy import engine_from_config, pool
 from alembic import context
 from models.tablas import MiClaseBase
 
+# Cargar variables del archivo .env
 load_dotenv()
 database_url = os.getenv("DATABASE_URL")
 
-target_metadata = MiClaseBase.metadata
-
+# Configuracio de Alembic
 config = context.config
+config.set_main_option("sqlalchemy.url", database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+target_metadata = MiClaseBase.metadata
 
 def run_migrations_offline() -> None:
-    url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
+        url=database_url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
-
     with context.begin_transaction():
         context.run_migrations()
-
 
 def run_migrations_online() -> None:
     connectable = engine_from_config(
@@ -39,17 +34,10 @@ def run_migrations_online() -> None:
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
-
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, 
-            target_metadata=target_metadata,
-           
-        )
-
+        context.configure(connection=connection, target_metadata=target_metadata)
         with context.begin_transaction():
             context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
